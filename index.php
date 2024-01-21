83236422290db88e402a1510438503ff57e9a467
apol yuzuguldu <vargakeri@icloud.com>
2024-01-21 21:24:43 +0100

    terza lezione php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+class Company {
+    public $name;
+    public $location;
+    public $totEmployees = 0;
+    public static $totalAvgAnnualWage = 0;
+    public static $avgWage = 1500;
+
+    public function __construct($_name, $_location, $_tot_employees)
+    {
+        $this->name = $_name;
+        $this->location = $_location;
+        $this->totEmployees = $_tot_employees;
+    }
+    
+    public function PrintDescription() {
+        if ($this->totEmployees > 1) {
+            echo "The company $this->name is located in $this->location and has $this->totEmployees employees.\n";
+        } else {
+            echo "The company $this->name is located in $this->location and does not have employees.\n";
+        }
+    }
+
+    public function AvgAnnualWage(){
+        $avg_annual_wage = self::$avgWage * $this->totEmployees * 12 ;
+        echo "$this->name's average annual spend is $avg_annual_wage\n";
+
+        self::$totalAvgAnnualWage += $avg_annual_wage;
+        
+    }
+
+    public function avgMonth($month){
+        return self::$avgWage * $this->totEmployees * $month;
+    }
+
+    public static function PrintTotal () {
+        echo "The total of all Avg Annual Wage is " .  self::$totalAvgAnnualWage . "\n";
+    }
+}
+
+$companies = [
+	new Company('Apple', 'Cupertino', 200),
+new Company('Google', 'California', 100), 
+	new Company('Tesla', 'USA', 150), 
+	new Company('Amazon', 'Seattle', 400), 
+	new Company('Microsoft', 'Redmond', 500)
+	];
+	
+foreach ($companies as $company) {
+    $company->PrintDescription();
+    $company->AvgAnnualWage();
+}
+
+echo $companies[1] -> avgMonth(readline ("Su quanti mesi vuoi calcolare le spese? : ")) . "\n";
+
+
+Company::PrintTotal();
